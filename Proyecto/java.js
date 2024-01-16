d7edb73572bd17b23d45dc400abc92aa40d83a05
osanchez-2019490 <osanchez-2019490@kinal.edu.gt>
2024-01-16 16:29:18 -0600

    Cuarto

--- Proyecto/java.js
+++ Proyecto/java.js
@@ -5,3 +5,19 @@ function palabra_alatoria(){
     aleat= Math.random() * (palabra.length)
     aleat = Math.floor(aleat)
 }
+
+var textA = document.getElementById('texto');
+var vidas = 0;
+
+var text1 = document.getElementById('texto1');
+var text2 = document.getElementById('texto2');
+var text3 = document.getElementById('texto3');
+var text4 = document.getElementById('texto4');
+var text5 = document.getElementById('texto5');
+var text6 = document.getElementById('texto6');
+
+function comprobar(){
+  for(i = 0; i < aleat.length, i++;){
+
+  }
+}
